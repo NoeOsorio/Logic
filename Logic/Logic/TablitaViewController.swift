628e8e7ccdc2f1cc929ac48ee304db4b7eea0574
Noe Osorio <osorionoe@gmail.com>
2018-05-18 03:41:40 -0500

    Tabla de verdad funcionando!

---
+++ Logic/Logic/TablitaViewController.swift
@@ -0,0 +1,71 @@
+//
+//  TablitaViewController.swift
+//  Logic
+//
+//  Created by Noe Osorio on 17/05/18.
+//  Copyright © 2018 Noe Osorio. All rights reserved.
+//
+
+import UIKit
+
+class TablitaViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        //Columna
+        //Atomo
+        return tabla.count + 1 //pow(2, matriz.count)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //Fila
+        //valor de verdad
+        return logica.todo.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "columna", for: indexPath) as! CollectionViewCell
+        
+        if(indexPath.section == 0){
+            cell.title.text = tabla[indexPath.section].logica.todo[indexPath.row].data.nombre
+            
+        }
+        else{
+            cell.title.text = String(tabla[indexPath.section-1].logica.todo[indexPath.row].data.estado)
+            if(tabla[indexPath.section-1].logica.todo[indexPath.row].data.estado){
+                cell.title.textColor = UIColor.green
+            }
+            else{
+                cell.title.textColor = UIColor.red
+            }
+        }
+        
+        
+        //cell.title.text = matriz[indexPath.section][indexPath.row]
+        //String(indexPath.row)
+        
+        
+        return cell
+    }
+    
+
+    
+    var logica = Logica()
+    var header: [String] = []
+    var tabla: [Tabla] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        logica.recorrer()
+        for formula in logica.todo{
+            header.append(formula.data.nombre)
+            
+        }
+        tabla = logica.tablita
+        
+    }
+
+    
+    
+
+
+}
